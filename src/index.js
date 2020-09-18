294ce9db025b8ee233621038f232c1373f742e71
Amr Fouad <68440701+3mrfouad@users.noreply.github.com>
2020-09-18 01:46:24 -0600

    Initial commit after 'npx create-react-app' and 'npm installed redux and uuid'

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Hello Scriptorce</h1>
+   <> </>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
