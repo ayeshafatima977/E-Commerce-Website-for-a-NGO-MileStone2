10022d1b1033147cf5c2d7cc924649c0c1005e6c
Kenji <kenji.au9@gmail.com>
2020-09-21 10:32:01 -0600

    Added Product Nav action file

---
+++ src/actions/Page-Navigation.js
@@ -0,0 +1,26 @@
+const Search = (userInput) => {
+  return {
+    type: "SEARCH_PRODUCT",
+    payload: userInput;
+  }
+}
+const Sort = (userInput) => {
+  return {
+    type: "SORT_PRODUCT",
+    payload: userInput;
+  }
+}
+const Browse = (userInput) => {
+  return {
+    type: "BROWSE_PRODUCT",
+    payload: userInput;
+  }
+}
+const Filter = (userInput) => {
+  return {
+    type: "FILTER_PRODUCT",
+    payload: userInput;
+  }
+}
+
+export {Search, Sort, Browse, Filter} ;
