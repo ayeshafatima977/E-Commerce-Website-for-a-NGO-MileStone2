d13816c559aafbe715691fd4043896c4b1189464
dhmec <dharmoradiya@gmail.com>
2020-09-21 10:25:08 -0600

    Add Contact us section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,22 @@ import React from 'react';
 =            Footer            =
 =============================================*/
 
-const Navigation = () => 
+const Footer = () => 
 {
     return(
-        <section>            
-            
-            
-        </section>
+        
+        /*=============================================
+        =            Contact Us            =
+        =============================================*/
+        <section>
+            <h1>Contact Us</h1>            
+            <p>12332 123 St Nw, Edmonton</p>
+            <p>Alberta, T0A 0W0, 780-000-8888</p>                  
+        </section>       
+        /*=====  End of Contact Us  ======*/
+        
+        
+        
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 
 ReactDOM.render(
   <React.StrictMode>
      <Navigation />
+     <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
