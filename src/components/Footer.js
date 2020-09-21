134ee14e8df744830868ea2f7455aaa08a600fdd
dhmec <dharmoradiya@gmail.com>
2020-09-21 10:18:30 -0600

    Created Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+
+/*=============================================
+=            Footer            =
+=============================================*/
+
+const Navigation = () => 
+{
+    return(
+        <section>            
+            
+            
+        </section>
+    );
+}
+
+/*=====  End of Footer  ======*/
+
+export default Footer;
