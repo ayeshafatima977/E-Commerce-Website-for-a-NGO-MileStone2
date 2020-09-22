5ee42093b9b391ef3ca974d031f23a329032c0fa
Kenji <kenji.au9@gmail.com>
2020-09-21 18:12:15 -0600

    Finished Cart Reducer logic cases

--- src/reducers/Cart.js
+++ src/reducers/Cart.js
@@ -17,20 +17,42 @@ const CartReducer = (state = [], action) => {
         }
       }
       // Once the index of the item is found, remove that item from the array
-      cartCopy.splice(i,1);
+      cartCopy.splice(i, 1);
       return cartCopy;
     }
     case "INCREASE_CART_QTY": {
-      // Logic for adding an items quantity by 1 in the cart
-      break;
+      for (let item of cartCopy) {
+        if (item.id === action.payload) {
+          item.inCartQty++;
+        }
+      }
+      return cartCopy;
     }
     case "DECREASE_CART_QTY": {
-      // Logic for removing an items quantity by 1 in the cart
-      break;
+      let i = -1;
+      let noItem = false;
+      for (let item of cartCopy) {
+        // increment the internal counter
+        i++;
+        if (item.id === action.payload) {
+          item.inCartQty--;
+          if (item.inCartQty === 0) {
+            noItem = true;
+          }
+        }
+      }
+      if (noItem) {
+        cartCopy.splice(i, 1);
+      }
+      return cartCopy;
     }
     case "SET_CART_QTY": {
-      // Logic for setting a items quantity in the cart
-      break;
+      for (let item of cartCopy) {
+        if (item.id === action.payloadOne) {
+          item.inCartQty = action.payloadTwo;
+        }
+      }
+      return cartCopy;
     }
     default:
       return state;
