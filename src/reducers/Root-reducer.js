f9b8e63e1f55e86503ef43f49054576fecf2bf59
Ayesha Fatima <ayeshafatima977@gmail.com>
2020-09-25 09:56:48 -0600

    Removed the conflicts;

--- src/reducers/Root-reducer.js
+++ src/reducers/Root-reducer.js
@@ -1,9 +1,9 @@
-import { combineReducers } from 'redux';
-import ShoppingNavigationReducer from './Shopping-navigation';
-import CartReducer from './Cart';
-import BillingInfoReducer from './Billing-info';
-import CreditCardReducer from './Credit-card';
-import BrowsingHistoryReducer from './Browsing-history';
+import { combineReducers } from "redux";
+import ShoppingNavigationReducer from "./Shopping-navigation";
+import CartReducer from "./Cart";
+import BillingInfoReducer from "./Billing-info";
+import CreditCardReducer from "./Credit-card";
+import BrowsingHistoryReducer from "./Browsing-history";
 
 const UserDataReducer = combineReducers({
   BillingInfo: BillingInfoReducer,
@@ -14,8 +14,7 @@ const GlobalReducer = combineReducers({
   ShopNav: ShoppingNavigationReducer,
   Cart: CartReducer,
   UserData: UserDataReducer,
-  BrowsingHistory: BrowsingHistoryReducer
-
+  BrowsingHistory: BrowsingHistoryReducer,
 });
 
 export default GlobalReducer;
