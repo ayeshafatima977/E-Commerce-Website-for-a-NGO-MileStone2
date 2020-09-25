e386f198903f74e59faaf93765e6f7b528c2a32e
Kenji <kenji.au9@gmail.com>
2020-09-25 09:56:28 -0600

    Fixed hero css link;

--- src/components/Hero-section.js
+++ src/components/Hero-section.js
@@ -1,26 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LandingPage from "../img/LandingPage.jpg";
-import "../css/Hero-section.css"
+import "../css/Hero-section.css";
 
 const HeroSectionComponent = () => {
   return (
     <>
-    <div className="hero-section">
-{/*       <img src={LandingPage} alt="A Kid eating Mango" />
- */}      <p>
-        We Strive To Make A Difference for Those Kids In Need.Empowering people
-        to rise out of poverty.
-      </p>
-      <p>
-        We bring real and lasting change to the lives of the world’s most
-        vulnerable children. For good.
-      </p>
-      <Link to="/donation">DONATE</Link>
-      {/* <input type="submit" id="donate" value="DONATE" ></input> */}
-      <Link to="/shop">SHOP</Link>
-      {/* <input type="submit" id="shop" value="SHOP"></input> */}
-      <p>Each Item You Purchase Feeds a Hungry Kid.Shop For a Purpose Today.</p>
+      <div className="hero-section">
+        {/* <img src={LandingPage} alt="A Kid eating Mango" /> */}
+        <p>
+          We Strive To Make A Difference for Those Kids In Need.Empowering
+          people to rise out of poverty.
+        </p>
+        <p>
+          We bring real and lasting change to the lives of the world’s most
+          vulnerable children. For good.
+        </p>
+        <Link to="/donation">DONATE</Link>
+        {/* <input type="submit" id="donate" value="DONATE" ></input> */}
+        <Link to="/shop">SHOP</Link>
+        {/* <input type="submit" id="shop" value="SHOP"></input> */}
+        <p>
+          Each Item You Purchase Feeds a Hungry Kid.Shop For a Purpose Today.
+        </p>
       </div>
     </>
   );
