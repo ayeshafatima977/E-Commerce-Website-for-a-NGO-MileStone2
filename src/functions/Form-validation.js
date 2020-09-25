10db4a42d63b5053d2b312e1120721240ce96163
dhmec <dharmoradiya@gmail.com>
2020-09-25 12:18:22 -0600

    Added Regular expression in form-validation.js

--- src/functions/Form-validation.js
+++ src/functions/Form-validation.js
@@ -61,7 +61,8 @@ export default function FormValidation(
     /* Last Name End */
 
     /* Email Start */
-
+    //  Citation : Email Validation RegExpression
+    // link : https://www.w3resource.com/javascript/form/email-validation.php
     case "email":
       console.log(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
         inputFieldContent));
@@ -112,6 +113,8 @@ export default function FormValidation(
 
     /* Subject End */
 
+    /* message End */
+
     case "message":
       if (inputFieldContent === "") {
         document
@@ -130,10 +133,12 @@ export default function FormValidation(
           .classList.remove("input-error");
       }
       return PassIndicator;
+    /* Message End */
 
-    /* Subject End */
-
-    /* Message Start */
+    /* Credit Cart Number Input Start */
+    //  Citation : Accept Digits Only RegExpression
+    // link : https://stackoverflow.com/questions/9011524/regex-to-check-whether-a-string-contains-only-numbers
+    
     case "cc-number-input":
       if (inputFieldContent === "" || !/^\d+$/.test(inputFieldContent)) {
         document
@@ -152,9 +157,16 @@ export default function FormValidation(
           .classList.remove("input-error");
       }
       return PassIndicator;
-    // @Link:https://stackoverflow.com/questions/15774555/efficient-regex-for-canadian-postal-code-function
+    
+    /* Credit Cart Number Input end */
+    
+    /* Postal code start */
+    
+    
+    // Citation : Postal code RegExpression for canadian postal code
+    // @Link:https://regexlib.com/Search.aspx?k=canadian+postal+code&AspxAutoDetectCookieSupport=1
     case "postal":
-      if (!/^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/.test(inputFieldContent)) {
+      if (!/^[A-Za-z][0-9][A-Za-z]{0,1}[0-9][A-Za-z][0-9]$/.test(inputFieldContent)) {
         document
           .getElementsByClassName(errorMessageClassName)[1]
           .classList.add("msg-show");
@@ -171,9 +183,14 @@ export default function FormValidation(
           .classList.remove("input-error");
       }
       return PassIndicator;
-
+    
+    /* Postal code end  */
+    
+    /* City Name start  */    
+    // Citation : City name RegExpression accepting space in the city name i.e san francisco
+    // @Link:https://stackoverflow.com/questions/11757013/regular-expressions-for-city-name
     case "city":
-      if (!/^[A-Za-z]+$/.test(inputFieldContent)) {
+      if (!/^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/.test(inputFieldContent)) {
         document
           .getElementsByClassName(errorMessageClassName)[1]
           .classList.add("msg-show");
@@ -190,7 +207,11 @@ export default function FormValidation(
           .classList.remove("input-error");
       }
       return PassIndicator;
-
+      
+    /* city name end */
+    
+    /* Address start */
+    // Address RegExpression   
     // @Link: https://stackoverflow.com/questions/3763820/javascript-regular-expression-to-validate-an-address
     case "address":
       if (!/^[a-zA-Z0-9\s,'-]*$/.test(inputFieldContent)) {
@@ -210,6 +231,35 @@ export default function FormValidation(
           .classList.remove("input-error");
       }
       return PassIndicator;
+    
+    /* Address End */
+    
+    /* Mobile Number Start */
+    // Citation :  Mobile Number RegExpression, Accept input in XXX-XXX-XXXX or XXX.XXX.XXXX or XXX XXX XXXX
+    // Link : https://www.w3resource.com/javascript/form/phone-no-validation.php
+    case "mobile":
+      if (!/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(inputFieldContent)) {
+        document
+          .getElementsByClassName(errorMessageClassName)[1]
+          .classList.add("msg-show");
+        document
+          .getElementsByClassName(inputFieldClassName)[1]
+          .classList.add("input-error");
+        PassIndicator = false;
+      } else {
+        document
+          .getElementsByClassName(errorMessageClassName)[0]
+          .classList.remove("msg-show");
+        document
+          .getElementsByClassName(inputFieldClassName)[0]
+          .classList.remove("input-error");
+      }
+      return PassIndicator;
+    /* Mobile Number end */
+    
+    
+    
+    
 
     default:
       return;
