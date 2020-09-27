1a539ac11916efa2178f47566069582fa3b66f92
Ayesha Fatima <ayeshafatima977@gmail.com>
2020-09-27 15:04:53 -0600

    Add the Thakyou-image and css styling;

---
+++ src/components/Thankyou-page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ThankYouImg from "../img/thankyou.jpg";
+import "../css/Thankyou-page.css";
+
+function ThankYouPage() {
+  return (
+    <div id="thankyou-img">
+      <img src={ThankYouImg} alt="Image for Thank You to the customer" />
+    </div>
+  );
+}
+
+export default ThankYouPage;
